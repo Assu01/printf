a8927a90e8605dd10a36bb8f6c8982ab91498f28
Asrat Asmelash <asratasse@gmail.com>
2022-04-17 21:10:18 +0300

    printf

--- _putchar.c
+++ _putchar.c
@@ -1,11 +1,11 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - Write a character.
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * @c: The character to be printed.
- *
- * Return: 1(Success) -1(Fail)
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
--- main.h
+++ main.h
@@ -1,46 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
-#include <stddef.h>
-/**
- * struct specifier - struct specifier.
- *
- * @valid: The valid character.
- * @f: The functions associated.
- */
-
-typedef struct specifier
-{
-	char *valid;
-	int (*f)(va_list);
-} spec;
-
-int _putchar(char c);
-int (*get_func(char x))(va_list args);
-int _printf(const char *format, ...);
-int print_c(va_list args);
-int print_s(va_list args);
-int print_d(va_list args);
-int print_i(va_list args);
-int print_percent(va_list args);
-int print_b(va_list args);
-int print_u(va_list args);
-int print_o(va_list args);
-int print_p(va_list args);
-int print_rev(va_list args);
-int print_R(va_list args);
-void print_pointer(unsigned long ptr, int *count);
-int print_x(va_list args);
-void print_lowerHex(unsigned int num, int *count);
-int print_X(va_list args);
-void print_upperHex(unsigned int num, int *count);
-int countDigits(unsigned int num);
-int countOctal(unsigned int num);
-int countBinary(unsigned int num);
-int print_number(unsigned int n);
-int _strlen(char *);
-
 #include <limits.h>
 #include <stdarg.h>
 #include <stdlib.h>
@@ -152,4 +112,5 @@ unsigned int convert_sbase(buffer_t *output, long int num, char *base,
 		unsigned char flags, int wid, int prec);
 unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
 		unsigned char flags, int wid, int prec);
+
 #endif
